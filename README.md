0c13e4c1660b5864623bbce5f18006eb39500ac8
venkat <venkatrathod55@gmail.com>
2022-04-07 20:26:35 +0530

    query parametr e

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Backend cohort Feb 2022 - May 2022
  ## GET /all-candidates
  Wrte a get api that returnsa list of candidate names
 
- ## GET /candidates
+ ## GET /candidates?count=3
  Write a get pi that returns a list of candidates with size equal to the count value recieved in the query param.
+
+ Consider that the count value is greater than 0 and less or eual 10.
 
  the request you will send should have a url like this - https://localhost:4000/candidates?count=3
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,15 +3,16 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('allcandidates', function (req, res) {
+router.get('/test-me', function (req, res) {
     console.log('------------------')
-    let arr=["anu","arun","lalu","kgf","srinu","yogi"]
-    res.send(arr)
+    console.log(req)
+    console.log('------------------')
+    console.log('These are the request query parameters: ', req.query)
+    res.send('My first ever api!')
 });
-/*router.get('candidates', function (req, res) {
-    console.log('------------------')
-    let arr=["anu","arun","lalu","kgf","srinu","yogi"]
-    let a=req.query.count
-    let result=arr.slice(o,a)
-    res.send(result)
-});*/
+
+
+
+
+module.exports = router;
+// adding this comment for no reason
